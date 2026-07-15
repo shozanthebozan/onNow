96b18f75a2580ea11f0912c0dd7436ba9144dedc
shozanthebozan <shozanthebozan@gmail.com>
2026-07-15 16:50:46 +1000

    Made it it works fine now

--- onNow_v0.0.1.py
+++ onNow_v0.0.1.py
@@ -1,6 +1,7 @@
 import webbrowser as wb
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import ttk
 import requests
 root=tk.Tk()
 root.geometry('500x200')
@@ -8,12 +9,23 @@ root.title("onNow")
 root.config(bg="dark slate gray")
 Title=tk.Label(root,text="OnNow",font=("Times New Roman", 15, "bold"),bg="light slate gray")
 Title.place(anchor="center",relx=0.5,rely=0.1)
+weather_result=ttk.Label(root)
+weather_result.place(anchor="w",rely=0.5,relx=0.1)
 def weatherFind():
     weather=requests.get("https://wttr.in/Melbourne?format=2")
-    weatherg=weather.text.strip()
-    print(f"{weatherg}")
+    global weatherg
+    weather=weather.text.strip()
+    clean_text = weather.replace("\ufe0f", "")
+    cleaner_text=clean_text.replace("+", "")
+    cleanest_text=cleaner_text.replace("🌬", ", & wind").replace("☁", "☁,").replace("→", " up to ")
+    
+    weatherg = cleanest_text
+    weather_result.config(text=f"Sky={weatherg}")
+    
+    
+    
+    
 weather_get=tk.Button(root, text="Get weather",command=weatherFind)
-weather_get.place(anchor="w",relx=0.1,rely=0.5) 
-
+weather_get.place(anchor="w",relx=0.1,rely=0.3) 
 
 root.mainloop()
