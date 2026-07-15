d7742fa25ce21bc138a5764556665170803aeda6
shozanthebozan <shozanthebozan@gmail.com>
2026-07-15 16:09:40 +1000

    It sends weather to terminal now time to print in tkinter gui!

--- onNow_v0.0.1.py
+++ onNow_v0.0.1.py
@@ -9,12 +9,11 @@ root.config(bg="dark slate gray")
 Title=tk.Label(root,text="OnNow",font=("Times New Roman", 15, "bold"),bg="light slate gray")
 Title.place(anchor="center",relx=0.5,rely=0.1)
 def weatherFind():
-    weather=requests.get("https://wttr.in/Melbourne?format=2?")
-    weatheref=weather.split('<div class="term-container">')
-    finalweather=weatheref.split('</div>')
-    print(f"{finalweather}")
+    weather=requests.get("https://wttr.in/Melbourne?format=2")
+    weatherg=weather.text.strip()
+    print(f"{weatherg}")
 weather_get=tk.Button(root, text="Get weather",command=weatherFind)
-weather_get.place(anchor="w",relx=0.1,rely=0.5)
+weather_get.place(anchor="w",relx=0.1,rely=0.5) 
 
 
 root.mainloop()
