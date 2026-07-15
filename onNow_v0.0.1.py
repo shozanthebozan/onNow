f0204422da79bc3d1aac891985b849e1bb6dea9f
shozanthebozan <shozanthebozan@gmail.com>
2026-07-15 15:07:37 +1000

    Added the basic interface so far

---
+++ onNow_v0.0.1.py
@@ -0,0 +1,9 @@
+import webbrowser as wb
+import tkinter as tk
+from tkinter import messagebox
+root=tk.Tk()
+root.geometry('500x200')
+root.title("onNow")
+Title=tk.Label(root,text="OnNow",font=("Times New Roman", 15, "bold"))
+Title.place(anchor="center",relx=0.5,rely=0.1)
+root.mainloop()
