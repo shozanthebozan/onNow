9a1c4eb7f0189721be94cf8a91bba4b48fcf2b8b
shozanthebozan <shozanthebozan@gmail.com>
2026-07-15 15:42:19 +1000

    Saving progress so far

--- onNow_v0.0.1.py
+++ onNow_v0.0.1.py
@@ -1,9 +1,18 @@
 import webbrowser as wb
 import tkinter as tk
 from tkinter import messagebox
+
 root=tk.Tk()
 root.geometry('500x200')
 root.title("onNow")
-Title=tk.Label(root,text="OnNow",font=("Times New Roman", 15, "bold"))
+root.config(bg="dark slate gray")
+Title=tk.Label(root,text="OnNow",font=("Times New Roman", 15, "bold"),bg="light slate gray")
 Title.place(anchor="center",relx=0.5,rely=0.1)
+def weatherFind():
+    weather=wb.open_new_tab("wttr.in/Melbourne?format=3")
+    print(f"{weather}")
+weather_get=tk.Button(root, text="Get weather",command=weatherFind)
+weather_get.place(anchor="w",relx=0.1,rely=0.5)
+
+
 root.mainloop()
