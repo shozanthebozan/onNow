be51a992a42812219ef50aec812fa4e503cfe756
shozanthebozan <shozanthebozan@gmail.com>
2026-07-16 06:54:16 +1000

    Removing version number from title and putting in __version__ variable instead

--- onNow.py
+++ onNow.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 import requests
+__version__="v0.0.4"
 root=tk.Tk()
 root.geometry('500x200')
 root.title("onNow")
@@ -25,5 +26,7 @@ def weatherFind():
     
 weather_get=tk.Button(root, text="Get weather",command=weatherFind,bg="light slate grey")
 weather_get.place(anchor="w",relx=0.1,rely=0.3) 
+version_label=tk.Label(root,text=f"{__version__}",bg="light slate grey")
+version_label.place(anchor="se",rely=1.0,relx=1.0)
 
 root.mainloop()
