6bd53f895e858b9b70cfe21f68dfc18aa2c3ddfb
shozanthebozan <shozanthebozan@gmail.com>
2026-07-15 15:53:27 +1000

    done adding weatrher now debugging

--- onNow_v0.0.1.py
+++ onNow_v0.0.1.py
@@ -1,7 +1,7 @@
 import webbrowser as wb
 import tkinter as tk
 from tkinter import messagebox
-
+import requests
 root=tk.Tk()
 root.geometry('500x200')
 root.title("onNow")
@@ -9,8 +9,10 @@ root.config(bg="dark slate gray")
 Title=tk.Label(root,text="OnNow",font=("Times New Roman", 15, "bold"),bg="light slate gray")
 Title.place(anchor="center",relx=0.5,rely=0.1)
 def weatherFind():
-    weather=wb.open_new_tab("wttr.in/Melbourne?format=3")
-    print(f"{weather}")
+    weather=requests.get("https://wttr.in/Melbourne?format=2?")
+    weatheref=weather.split('<div class="term-container">')
+    finalweather=weatheref.split('</div>')
+    print(f"{finalweather}")
 weather_get=tk.Button(root, text="Get weather",command=weatherFind)
 weather_get.place(anchor="w",relx=0.1,rely=0.5)
 
