d4fd1fd9f0356862bedfe2aa114a8b7b97aac92e
shozanthebozan <shozanthebozan@gmail.com>
2026-07-19 12:33:14 +1000

    Saving changes still gotta debug and clean tho

--- onNow.py
+++ onNow.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 import requests
-__version__="v0.0.5"
+__version__="v0.0.6"
 root=tk.Tk()
 root.geometry('500x200')
 root.title("onNow")
@@ -10,6 +10,8 @@ Title=tk.Label(root,text="OnNow",font=("Times New Roman", 15, "bold"),bg="dark s
 Title.place(anchor="center",relx=0.5,rely=0.1)
 weather_result=tk.Label(root,bg="light slate grey")
 weather_result.place(anchor="w",rely=0.5,relx=0.1)
+weather_result_sun=tk.Label(root,bg="light slate grey")
+weather_result_sun.place(anchor="w",rely=0.6,relx=0.1)
 def weatherFind():
     weather=requests.get("https://wttr.in/?format=2")
     global weatherg
@@ -17,6 +19,26 @@ def weatherFind():
     clean_text = weather.replace("\ufe0f", "").replace("🌬", ", & wind direction & speed is ").replace("☁", "☁,").replace("→", " up to ").replace("+", "")    
     weatherg = clean_text
     weather_result.config(text=f"Sky={weatherg}")
+    extra=requests.get("https://v2.wttr.in")
+    extrac=extra.text.strip()
+    extracclean=extrac.replace("\ufe0f", "").split("\n")
+    weather.placehold=""
+    timezone.placehold=""
+    # keep making these placeholder variables and then add em to for like if "Weather:" in line:
+                                                                            # weather_placehold = line.strip()
+    for line in extracclean:
+        if "Weather:" in line:
+            line=(line.strip())
+        elif "Timezone:" in line:
+            print(line.strip())
+        elif "Now  :" in line:
+            print(line.strip())
+        elif "Weather report:" in line:
+            print(line.strip())
+    
+    
+    
+    print(f"{extracclean}")
     
     
     
