f9e27d47a940fd15083e62fa774a1ec82f4d8313
shozanthebozan <shozanthebozan@gmail.com>
2026-07-26 18:36:48 +1000

    Adding a CLI version

---
+++ onNow_CLI.py
@@ -0,0 +1,44 @@
+import requests
+__Version__="v0.0.6"
+def weatherFind():
+    weather=requests.get("https://wttr.in/?format=2")
+    global weatherg
+    weather=weather.text.strip()
+    clean_text = weather.replace("\ufe0f", "").replace("🌬", ", & wind direction & speed is ").replace("☁", "☁,").replace("→", " up to ").replace("+", "")    
+    weatherg = clean_text
+    extra=requests.get("https://v2.wttr.in")
+    extrac=extra.text.strip()
+    extracclean=extrac.replace("\ufe0f", "").split("\n")
+    print(f"Sky={weatherg}\n")
+    weather_placehold=""
+    timezone_placehold=""
+    now_placehold=""
+    weatherReport_placehold=""
+    sunrise_placeholder = "" 
+    sunset_placeholder = ""
+    for line in extracclean:
+            cleaned_line = line.strip()
+            if not cleaned_line:
+                continue
+                
+            if "─" in cleaned_line or "┌" in cleaned_line or "┐" in cleaned_line or "└" in cleaned_line or "┘" in cleaned_line:
+                continue
+            if "Weather:" in cleaned_line:
+                weather_placeholder = cleaned_line
+            elif "Timezone:" in cleaned_line:
+                timezone_placeholder = cleaned_line
+            elif "Now :" in cleaned_line:
+                now_placeholder = cleaned_line
+            elif "Weather report:" in cleaned_line:
+                weatherReport_placeholder = cleaned_line
+            elif "Sunrise:" in cleaned_line:
+                sunrise_placeholder = cleaned_line
+            elif "Sunset:" in cleaned_line:
+                sunset_placeholder = cleaned_line
+    sunset_placeholder=sunset_placeholder.replace("\ufe0f", "").replace("\x1b", "").replace("\033", "").replace("[2m","").replace("[0m","")
+    sunrise_placeholder=sunrise_placeholder.replace("\ufe0f", "").replace("\x1b", "").replace("\033", "").replace("[2m","").replace("[0m","")
+    sunrise_placeholder2 = sunrise_placeholder.split("|", 1)[1]
+    print(f"{sunrise_placeholder2}\n{sunset_placeholder}")
+
+if __name__ == "__main__":
+    weatherFind()
